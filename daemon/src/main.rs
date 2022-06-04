5bc790776f71906586f675732f9fcd4eb479f8d0
Jack Hogan <jackhogan11@gmail.com>
2022-06-04 11:27:45 -0400

    Finished initial Pulse code

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,10 +1,34 @@
+use std::{thread, time::Duration};
+
 use tuxphones::pulse::PulseHandle;
 
 fn main() {
+    // test_pulse();
+}
+
+fn test_pulse() {
     println!("Hello, world!");
     let mut handle = PulseHandle::new().expect("Failed!");
+
+    println!("Sinks ====");
     let sinks = handle.get_sinks();
     sinks.into_iter().for_each(|f| {
-        println!("{}", f.name);
+        println!("{} (IDX: {})", f.name, f.index);
     });
+
+    println!("Audio Applications ====");
+    let audio_apps = handle.get_audio_applications();
+    audio_apps.into_iter().for_each(|f | {
+        println!("{} (PID: {})", f.name, f.pid);
+    });
+
+    println!("Setup capture ====");
+    handle.setup_audio_capture(Some("alsa_output.pci-0000_0d_00.4.analog-stereo")).expect("Failed to setup capture!");
+
+    println!("Start capture ====");
+    handle.start_capture(6).expect("Failed to start capture!");
+    thread::sleep(Duration::from_secs(5));
+
+    println!("Stop capture ====");
+    handle.stop_capture();
 }
--- daemon/src/pulse.rs
+++ daemon/src/pulse.rs
@@ -1,20 +1,33 @@
 use std::{rc::Rc, cell::RefCell, ops::Deref};
 
-use libpulse_binding::{mainloop::threaded::Mainloop, context::{Context, State, FlagSet as ContextFlagSet}, callbacks::ListResult};
+use libpulse_binding::{mainloop::threaded::Mainloop, context::{Context, State, FlagSet as ContextFlagSet}, callbacks::ListResult, operation::Operation};
 
 pub struct PulseHandle {
     mainloop: Rc<RefCell<Mainloop>>,
-    context: Rc<RefCell<Context>>
+    context: Rc<RefCell<Context>>,
+    audio_is_setup: bool,
+    tuxphones_sink_module_index: Option<u32>,
+    combined_sink_index: Option<u32>,
+    combined_sink_module_index: Option<u32>,
+    current_app_info: Option<CurrentAppInfo>
+}
+
+struct CurrentAppInfo {
+    sink_input_restore_index: u32,
+    index: u32
 }
 
 pub struct BasicSinkInfo {
     pub name: String,
-    pub index: u32
+    pub index: u32,
+    module: Option<u32>
 }
 
 pub struct AudioApplication {
     pub name: String,
-    pub pid: usize
+    pub pid: usize,
+    pub index: u32,
+    pub sink_index: u32
 }
 
 #[derive(Debug)]
@@ -26,8 +39,25 @@ pub enum PulseInitializationError {
     ContextStateErr
 }
 
+#[derive(Debug)]
+pub enum PulseCaptureSetupError {
+    NoPassthrough,
+    NoDefaultSink
+}
+
+#[derive(Debug)]
+pub enum PulseCaptureError {
+    NotSetup,
+    FailedToMoveSinkInput,
+    NoAppWithPid
+}
+
 impl Drop for PulseHandle {
     fn drop(&mut self) {
+        if self.audio_is_setup {
+            self.teardown_audio_capture();
+        }
+
         self.mainloop.borrow_mut().lock();
         self.context.borrow_mut().disconnect();
         self.mainloop.borrow_mut().unlock();
@@ -97,13 +127,21 @@ impl PulseHandle {
 
         mainloop.borrow_mut().unlock();
 
-        Ok(PulseHandle { context: Rc::clone(&context), mainloop: Rc::clone(&mainloop) })
+        Ok(PulseHandle { 
+            context: Rc::clone(&context), 
+            mainloop: Rc::clone(&mainloop), 
+            audio_is_setup: false, 
+            tuxphones_sink_module_index: None, 
+            combined_sink_index: None,
+            combined_sink_module_index: None,
+            current_app_info: None
+        })
     }
 
     /// Gets the sinks connected to the Pulse server
     pub fn get_sinks(self: &mut Self) -> Vec<BasicSinkInfo> {
         self.mainloop.borrow_mut().lock();
-        let results: Rc<RefCell<Option<Vec<BasicSinkInfo>>>> = Rc::new(RefCell::new(Some(vec![])));
+        let results = Rc::new(RefCell::new(Some(vec![])));
 
         let ml_ref = Rc::clone(&self.mainloop);
         let results_ref = Rc::clone(&results);
@@ -111,7 +149,8 @@ impl PulseHandle {
             match res {
                 ListResult::Item(info) => results_ref.borrow_mut().as_mut().unwrap().push(BasicSinkInfo { 
                     name: info.name.as_ref().map_or(String::from("unknown name"), |n| { n.to_string() }), 
-                    index: info.index 
+                    index: info.index,
+                    module: info.owner_module
                 }),
                 ListResult::End | ListResult::Error => unsafe {
                     (*ml_ref.as_ptr()).signal(false);
@@ -119,13 +158,7 @@ impl PulseHandle {
             }
         });
 
-        // Wait for operation to complete
-        loop {
-            match op.get_state() {
-                libpulse_binding::operation::State::Running => self.mainloop.borrow_mut().wait(),
-                libpulse_binding::operation::State::Done | libpulse_binding::operation::State::Cancelled => break
-            }
-        }
+        op_wait(&mut self.mainloop.borrow_mut(), &op);
 
         self.mainloop.borrow_mut().unlock();
 
@@ -134,7 +167,214 @@ impl PulseHandle {
     } 
 
     /// Gets all applications that are producing audio
-    pub fn get_audio_applications() -> Vec<AudioApplication> {
-        vec![]
+    pub fn get_audio_applications(self: &mut Self) -> Vec<AudioApplication> {
+        self.mainloop.borrow_mut().lock();
+
+        let results = Rc::new(RefCell::new(Some(vec![])));
+
+        let ml_ref = Rc::clone(&self.mainloop);
+        let results_ref = Rc::clone(&results);
+        let op = self.context.borrow_mut().introspect().get_sink_input_info_list(move |res| {
+            match res {
+                ListResult::Item(info) => {
+                    if let Some(pid) = info.proplist.get_str("application.process.id") {
+                        results_ref.borrow_mut().as_mut().unwrap().push(AudioApplication {
+                            name: format!("{}:{}", info.proplist.get_str("application.name").unwrap_or("NONAME".to_string()), info.proplist.get_str("media.name").unwrap_or("NOMEDIA".to_string())),
+                            pid: pid.parse().unwrap(),
+                            index: info.index,
+                            sink_index: info.sink,
+                        });
+                    }
+                },
+                ListResult::End | ListResult::Error => unsafe {
+                    (*ml_ref.as_ptr()).signal(false);
+                }
+            }
+        });
+
+        op_wait(&mut self.mainloop.borrow_mut(), &op);
+
+        self.mainloop.borrow_mut().unlock();
+        
+        let res = results.borrow_mut().take().unwrap();
+        res
+    }
+
+    /// Adds sinks for audio capture
+    pub fn setup_audio_capture(self: &mut Self, passthrough_override: Option<&str>) -> Result<(), PulseCaptureSetupError> {
+        let passthrough_sink = match passthrough_override {
+            Some(s) => s.to_string(),
+            None => {
+                self.mainloop.borrow_mut().lock();
+                let result: Rc<RefCell<Option<String>>> = Rc::new(RefCell::new(None));
+                let ml_ref = Rc::clone(&self.mainloop);
+                let res_ref = Rc::clone(&result);
+                let op = self.context.borrow_mut().introspect().get_sink_info_by_name("@DEFAULT_SINK@", move |info| unsafe {
+                    match info {
+                        ListResult::Item(sink) => res_ref.borrow_mut().replace(sink.name.as_ref().map_or(String::from("unknown name"), |n| { n.to_string() })),
+                        ListResult::End | ListResult::Error => None
+                    };
+                    
+                    (*ml_ref.as_ptr()).signal(false);
+                });
+
+                op_wait(&mut self.mainloop.borrow_mut(), &op);
+                self.mainloop.borrow_mut().unlock();
+
+                let res = match result.borrow_mut().take() {
+                    Some(r) => r,
+                    None => return Err(PulseCaptureSetupError::NoDefaultSink)
+                };
+                res
+            }
+        };
+
+        let mut tux_sink_found = false;
+        let mut tux_combined_sink_found = false;
+        let mut passthrough_sink_found = false;
+
+        for sink in self.get_sinks() {
+            match &sink.name[..] {
+                "tuxphones" => tux_sink_found = true,
+                "tuxphones-combined" => tux_combined_sink_found = true,
+                n if n == passthrough_sink => passthrough_sink_found = true,
+                _ => {}
+            }
+        }
+
+        if !passthrough_sink_found {
+            return Err(PulseCaptureSetupError::NoPassthrough);
+        }
+
+        self.mainloop.borrow_mut().lock();
+        if !tux_sink_found {
+            let ml_ref = Rc::clone(&self.mainloop);
+            let op = self.context.borrow_mut().introspect().load_module(
+                "module-null-sink", 
+                "sink_name=tuxphones sink_properties=device.description=tuxphones", 
+                move |_| unsafe {
+                    (*ml_ref.as_ptr()).signal(false);
+                }
+            );
+
+            op_wait(&mut self.mainloop.borrow_mut(), &op);
+        }
+
+        if !tux_combined_sink_found {
+            let ml_ref = Rc::clone(&self.mainloop);
+            let op = self.context.borrow_mut().introspect().load_module(
+                "module-combine-sink", 
+                &format!("sink_name=tuxphones-combined sink_properties=device.description=tuxphones-combined slaves=tuxphones,{}", passthrough_sink), 
+                move |_| unsafe {
+                    (*ml_ref.as_ptr()).signal(false);
+                }
+            );
+
+            op_wait(&mut self.mainloop.borrow_mut(), &op);
+        }
+
+        self.mainloop.borrow_mut().unlock();
+
+        for sink in self.get_sinks() {
+            match &sink.name[..] {
+                "tuxphones" => self.tuxphones_sink_module_index = Some(sink.module.unwrap()),
+                "tuxphones-combined" => {
+                    self.combined_sink_module_index = Some(sink.module.unwrap());
+                    self.combined_sink_index = Some(sink.index);
+                },
+                _ => {}
+            }
+        }
+
+        self.audio_is_setup = true;
+        Ok(())
+    }
+
+    /// Removes audio capture sinks
+    pub fn teardown_audio_capture(self: &mut Self) {
+        self.audio_is_setup = false;
+
+        self.mainloop.borrow_mut().lock();
+
+        if let Some(idx) = self.tuxphones_sink_module_index {
+            self.unload_module(idx);
+        }
+
+        if let Some(idx) = self.combined_sink_module_index {
+            self.unload_module(idx);
+        }
+
+        self.tuxphones_sink_module_index = None;
+        self.combined_sink_index = None;
+        self.combined_sink_module_index = None;
+
+        self.mainloop.borrow_mut().unlock();
+    }
+
+    /// Unloads modules
+    fn unload_module(self: &mut Self, idx: u32) {
+        let ml_ref = Rc::clone(&self.mainloop);
+        let op = self.context.borrow_mut().introspect().unload_module(idx, move |_| unsafe {
+            (*ml_ref.as_ptr()).signal(false);
+        });
+
+        op_wait(&mut self.mainloop.borrow_mut(), &op);
+    }
+
+    /// Starts capturing audio from the application with the given Pulse PID
+    pub fn start_capture(self: &mut Self, pid: usize) -> Result<(), PulseCaptureError> {
+        if !self.audio_is_setup || self.combined_sink_module_index.is_none() {
+            return Err(PulseCaptureError::NotSetup);
+        }
+
+        for app in self.get_audio_applications() {
+            if app.pid == pid {
+                self.mainloop.borrow_mut().lock();
+
+                let ml_ref = Rc::clone(&self.mainloop);
+                self.current_app_info = Some(CurrentAppInfo {
+                    sink_input_restore_index: app.sink_index,
+                    index: app.index
+                });
+                let op = self.context.borrow_mut().introspect().move_sink_input_by_index(app.index, self.combined_sink_index.unwrap(), Some(Box::new(move |_| unsafe {
+                    (*ml_ref.as_ptr()).signal(false);
+                })));
+
+                op_wait(&mut self.mainloop.borrow_mut(), &op);
+
+                self.mainloop.borrow_mut().unlock();
+
+                return Ok(());
+            }
+        }
+
+        Err(PulseCaptureError::NoAppWithPid)
+    }
+
+    /// Stop capturing audio from application
+    pub fn stop_capture(self: &mut Self) {
+        self.mainloop.borrow_mut().lock();
+
+        if let Some(info) = &self.current_app_info {
+            let ml_ref = Rc::clone(&self.mainloop);
+            let op = self.context.borrow_mut().introspect().move_sink_input_by_index(info.index, info.sink_input_restore_index, Some(Box::new(move |_| unsafe {
+                (*ml_ref.as_ptr()).signal(false);
+            })));
+
+            op_wait(&mut self.mainloop.borrow_mut(), &op);
+        }
+
+        self.current_app_info = None;
+        self.mainloop.borrow_mut().unlock();
+    }
+}
+
+/// Wait for operation to complete
+fn op_wait<T: ?Sized>(ml: &mut Mainloop, op: &Operation<T>) {
+    loop {
+        match op.get_state() {
+            libpulse_binding::operation::State::Running => ml.wait(),
+            libpulse_binding::operation::State::Done | libpulse_binding::operation::State::Cancelled => break
+        }
     }
 }
