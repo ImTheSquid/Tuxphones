fe25187192221fe16005751c1995cd0ed411f9fe
DreamingCodes <me@dreaming.codes>
2022-06-12 17:12:32 +0200

    OpCode2 and OpCode8

--- daemon/src/discord.rs
+++ daemon/src/discord.rs
@@ -40,9 +40,12 @@ pub mod websocket {
 
     use crate::discord::api::{ApiError, get_ws_endpoint};
 
+    const API_VERSION: u8 = 7;
+
     #[derive(serde::Serialize, serde::Deserialize, Debug)]
     pub struct WebsocketMessage {
         pub op: u8,
+        // TODO: find a way to tell serde to deserialize this based on the op
         pub d: WebsocketMessageD,
     }
 
@@ -50,25 +53,61 @@ pub mod websocket {
     #[serde(untagged)]
     pub enum WebsocketMessageD {
         OpCode0(OpCode0),
+        OpCode2(OpCode2),
+        OpCode8(OpCode8),
     }
 
     #[derive(serde::Serialize, serde::Deserialize, Debug)]
-    pub struct OpCode0stream {
+    pub struct GatewayStream {
+        //Opcode 0 and 2 params
         #[serde(rename = "type")]
         pub stream_type: String,
         pub rid: u8,
-        pub quantity: u8,
+        pub quality: u8,
+
+        //Only opcode 2 params
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub active: Option<bool>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub ssrc: Option<u32>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub rtx_ssrc: Option<u32>
     }
 
     #[derive(serde::Serialize, serde::Deserialize, Debug)]
     pub struct OpCode0 {
         pub server_id: String,
         pub session_id: String,
-        pub streams: Vec<OpCode0stream>,
+        pub streams: Vec<GatewayStream>,
+        /// session token
         pub token: String,
         pub user_id: String,
         pub video: bool,
     }
+
+    /// Incoming message containing configuration options for websocket connection
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    pub struct OpCode2 {
+        experiment: Vec<String>,
+        /// Discord ip address to stream to
+        ip: String,
+        /// Discord port to stream to
+        port: u16,
+        /// Supported encrpytion modes by the server
+        modes: Vec<String>,
+        ssrc: u32,
+        streams: Vec<GatewayStream>,
+    }
+
+    /// Initial heartbeat incoming message
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    pub struct OpCode8 {
+        /// the interval (in milliseconds) the client should heartbeat with
+        heartbeat_interval: u32,
+        /// api version
+        v: u8,
+    }
+
 
     #[derive(Debug)]
     pub enum WebsocketInitError {
@@ -98,7 +137,7 @@ pub mod websocket {
         #[tracing::instrument]
         pub async fn new() -> Result<Self, WebsocketInitError> {
             //v7 is going to be deprecated according to discord's docs (https://www.figma.com/file/AJoBnWrHIFxjeppBRVfqXP/Discord-stream-flow?node-id=48%3A87) but is the one that discord client still use for video streams
-            let endpoint = format!("{}/?v=7", get_ws_endpoint().await?.url);
+            let endpoint = format!("{}/?v={}", get_ws_endpoint().await?.url, API_VERSION);
             info!("Connecting to {}", endpoint);
 
             let (ws_stream, response) = connect_async(endpoint).await?;
@@ -121,10 +160,14 @@ pub mod websocket {
                 d: WebsocketMessageD::OpCode0(OpCode0 {
                     server_id,
                     session_id,
-                    streams: vec![OpCode0stream {
+                    streams: vec![GatewayStream {
                         stream_type: "video".to_string(),
                         rid: 100,
-                        quantity: 100,
+                        quality: 100,
+
+                        active: None,
+                        ssrc: None,
+                        rtx_ssrc: None
                     }],
                     token,
                     user_id,
