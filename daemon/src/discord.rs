6461a56d622be64da90620f5b575d56553672d11
DreamingCodes <me@dreaming.codes>
2022-06-12 14:12:58 +0200

    OpCode0 serde Deserialize and Serialize

--- daemon/src/discord.rs
+++ daemon/src/discord.rs
@@ -5,24 +5,24 @@ pub mod api{
     }
 
     #[derive(Debug)]
-    pub enum GetWsEndpointResponseError {
+    pub enum ApiError {
         ReqwestError(reqwest::Error),
         ParseError(serde_json::Error),
     }
 
-    impl From<reqwest::Error> for GetWsEndpointResponseError {
-        fn from(error: reqwest::Error) -> GetWsEndpointResponseError {
-            GetWsEndpointResponseError::ReqwestError(error)
+    impl From<reqwest::Error> for ApiError {
+        fn from(error: reqwest::Error) -> ApiError {
+            ApiError::ReqwestError(error)
         }
     }
 
-    impl From<serde_json::Error> for GetWsEndpointResponseError {
-        fn from(error: serde_json::Error) -> GetWsEndpointResponseError {
-            GetWsEndpointResponseError::ParseError(error)
+    impl From<serde_json::Error> for ApiError {
+        fn from(error: serde_json::Error) -> ApiError {
+            ApiError::ParseError(error)
         }
     }
 
-    pub async fn get_ws_endpoint() -> Result<GetWsEndpointResponse, GetWsEndpointResponseError> {
+    pub async fn get_ws_endpoint() -> Result<GetWsEndpointResponse, ApiError> {
         let body = reqwest::get("https://discord.com/api/gateway").await?.text().await?;
         let deserialized: GetWsEndpointResponse = serde_json::from_str(&body)?;
 
@@ -31,27 +31,87 @@ pub mod api{
 }
 
 pub mod websocket{
-    use tokio_tungstenite::connect_async;
-    use tracing::{debug};
-    use crate::discord::api::get_ws_endpoint;
+    use tokio::net::TcpStream;
+    use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
+    use tracing::{debug, info};
+    use tracing::field::debug;
+    use crate::discord::api::{get_ws_endpoint, ApiError};
 
-    pub struct websocket_connection {
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    pub struct WebsocketMessage {
+        pub op: u8,
+        pub d: WebsocketMessageD,
     }
 
-    impl websocket_connection {
-        #[tracing::instrument]
-        pub async fn new() -> Result<websocket_connection, tokio_tungstenite::tungstenite::Error> {
-            debug!("Connecting");
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    #[serde(untagged)]
+    pub enum WebsocketMessageD {
+        OpCode0(OpCode0),
+    }
 
-            let (ws_stream, response) = connect_async(format!("{:?}/?v=7", get_ws_endpoint().await.unwrap().url)).await?;
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    pub struct OpCode0stream {
+        #[serde(rename = "type")]
+        pub stream_type: String,
+        pub rid: u8,
+        pub quantity: u8,
+    }
+
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    pub struct OpCode0 {
+        pub server_id: String,
+        pub session_id: String,
+        pub streams: Vec<OpCode0stream>,
+        pub token: String,
+        pub user_id: String,
+        pub video: bool,
+    }
+
+    #[derive(Debug)]
+    pub enum WebsocketInitError {
+        ApiError(ApiError),
+        TungsteniteError(tokio_tungstenite::tungstenite::Error),
+    }
+
+    impl From<ApiError> for WebsocketInitError {
+        fn from(error: ApiError) -> WebsocketInitError {
+            WebsocketInitError::ApiError(error)
+        }
+    }
+
+
+    impl From<tokio_tungstenite::tungstenite::Error> for WebsocketInitError {
+        fn from(error: tokio_tungstenite::tungstenite::Error) -> WebsocketInitError {
+            WebsocketInitError::TungsteniteError(error)
+        }
+    }
+
+    pub struct WebsocketConnection {
+        ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
+    }
+
+    impl WebsocketConnection {
+        #[tracing::instrument]
+        pub async fn new() -> Result<Self, WebsocketInitError> {
+            //v7 is going to be deprecated according to discord's docs (https://www.figma.com/file/AJoBnWrHIFxjeppBRVfqXP/Discord-stream-flow?node-id=48%3A87) but is the one that discord client still use for video streams
+            let endpoint = format!("{}/?v=7", get_ws_endpoint().await?.url);
+            info!("Connecting to {}", endpoint);
+
+            let (ws_stream, response) = connect_async(endpoint).await?;
 
             debug!("Connected with response code: {:?}", response.status());
 
-
-
-
-            Ok(websocket_connection {
+            Ok(Self {
+                ws_stream
             })
+        }
+
+        /// auth the websocket connection using opcode 0
+        /// https://www.figma.com/file/AJoBnWrHIFxjeppBRVfqXP/Discord-stream-flow?node-id=48%3A87
+        #[tracing::instrument]
+        pub async fn auth(server_id: &str, session_id: &str, token: &str, user_id: &str) {
+
+            "".to_string();
         }
     }
 }
