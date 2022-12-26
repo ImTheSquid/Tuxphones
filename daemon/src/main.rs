e656da5808a384edd22deb9d9f380198168faa1e
DreamingCodes <dev@dreaming.codes>
2022-12-26 02:15:15 +0100

    Added logging categorization

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -2,20 +2,24 @@ use std::{
     panic,
     process,
     sync::{
-        atomic::{AtomicBool, Ordering},
         Arc,
+        atomic::{AtomicBool, Ordering},
     },
     time::Duration,
 };
-use tokio::sync::Mutex;
+
 use tokio::{
     signal::{ctrl_c, unix::SignalKind},
     sync::mpsc,
 };
+use tokio::sync::Mutex;
 use tracing::{error, info, Level};
+use tracing::level_filters::LevelFilter;
 use tracing_log::LogTracer;
-use tracing_subscriber::FmtSubscriber;
-use tuxphones::{socket::WebSocket, CommandProcessor};
+use tracing_subscriber::{filter, Layer};
+use tracing_subscriber::layer::SubscriberExt;
+
+use tuxphones::{CommandProcessor, socket::WebSocket};
 
 #[tokio::main]
 async fn main() {
@@ -36,11 +40,44 @@ async fn main() {
     }
 
     // Only set GST_DEBUG if not set already
-    if let Err(_) = std::env::var("GST_DEBUG") {
+    if std::env::var("GST_DEBUG").is_err() {
         std::env::set_var("GST_DEBUG", gst_level.to_string());
     }
 
-    let subscriber = FmtSubscriber::builder().with_max_level(log_level).finish();
+    //Filter to show only untargeted logs
+    let tuxphones_target_filter = filter::filter_fn(|meta| {
+        meta.target() == "tuxphones"
+    });
+
+    // Stdout logging
+    let stdout_log = tracing_subscriber::fmt::layer().pretty();
+
+    // Generic log file
+    let (non_blocking, _guard) = tracing_appender::non_blocking(std::fs::File::create("tux.log").unwrap());
+    let generic_log = tracing_subscriber::fmt::layer()
+        .with_ansi(false)
+        .with_writer(non_blocking)
+        .with_filter(tuxphones_target_filter.clone());
+
+    // SDP log file
+    let (non_blocking, _guard) = tracing_appender::non_blocking(std::fs::File::create("sdp.log").unwrap());
+    let sdp_log = tracing_subscriber::fmt::layer()
+        .with_ansi(false)
+        .with_writer(non_blocking)
+        .with_target(false)
+        .with_filter(filter::filter_fn(|meta| {
+            meta.target() == "sdp"
+        }));
+
+    let subscriber = tracing_subscriber::registry()
+        .with(stdout_log
+            .with_filter(
+                LevelFilter::from_level(log_level)
+            )
+            .with_filter(tuxphones_target_filter)
+        )
+        .with(generic_log)
+        .with(sdp_log);
 
     match tracing::subscriber::set_global_default(subscriber) {
         Ok(_) => {}
